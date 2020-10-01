a08bab5acfdb7deb7eb5ab83906048665d6b9f4a
Vlad <jake0_0@mail.ru>
2020-10-02 01:25:15 +0300

    turn all functions in personalMovieDB methods

--- js/petProject.js
+++ js/petProject.js
@@ -1,71 +1,80 @@
-let numberOfFilms;
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+
+    start: () => {
+        personalMovieDB.count = +prompt("number of watched films ?", "");
+        while (personalMovieDB.count == '' || personalMovieDB.count == null ||
+            isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("number of watched films ?", "");
+        }
+    },
+
+    writeYourGenres: () => {
+        for (let index = 0; index < 1; index++) {
+            let userGanre = prompt(
+                `Your favorite genres separated by coma`, '').toLowerCase();
+            if (userGanre == null ||
+                userGanre == '') {
+                index--; 
+            } else { 
+                personalMovieDB.genres = userGanre.split(', '); 
+                personalMovieDB.genres.sort();
+            }
+        }
+    },
+
+    showYourGenres: () => {
+        personalMovieDB.genres.forEach((element, index) => {
+            console.log(`Favorite genre #${index+1} - ${element}`);
+        });
+    },
+
+    toggleVisebleMyDB: () => {
+        personalMovieDB.privat = !(personalMovieDB.privat);
+    },
+
+    showMyDB: () => {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    rememberMyFilm: () => {
+
+        let bool = true,
+            lastFilm = "",
+            rateOfLastFilm = "";
+
+        while (bool != false) {
+
+            lastFilm = prompt(
+                "What the last film you watched", "");
+            rateOfLastFilm = prompt(
+                "Please rate your last film", "");
+
+            bool = (lastFilm == "") ? true :
+                (lastFilm.length <= 50 && lastFilm != null) ? false : true;
+        }
+
+        personalMovieDB.movies[lastFilm] = rateOfLastFilm;
+    },
+
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10) {
+            alert("You watched a small amaunt of films");
+        } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+            alert("You typical viewer");
+        } else if (personalMovieDB.count > 30) {
+            alert("Woww you such a big lover of cinema");
+        } else {
+            alert("Error");
+        }
+    },
 };
-
-personalMovieDB.count = start();
-rememberMyFilm();
-writeYourGenres();
-showMyDB();
-detectPersonalLevel();
-
-
-
-function writeYourGenres() {
-    for (let index = 0; index < 3; index++) {
-        personalMovieDB.genres[index] = prompt(
-            `Your favorite genre on number ${index+1}`);
-    }
-}
-
-function showMyDB() {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    }
-}
-
-function start() {
-    numberOfFilms = +prompt("number of watched films ?", "");
-    while (numberOfFilms == '' || numberOfFilms == null ||
-        isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("number of watched films ?", "");
-    }
-    return numberOfFilms;
-}
-
-function rememberMyFilm() {
-
-    let bool = true,
-        lastFilm = "",
-        rateOfLastFilm = "";
-
-    while (bool != false) {
-
-        lastFilm = prompt(
-            "What the last film you watched", "");
-        rateOfLastFilm = prompt(
-            "Please rate your last film", "");
-
-        bool = (lastFilm == "") ? true :
-            (lastFilm.length <= 50 && lastFilm != null) ? false : true;
-    }
-
-    personalMovieDB.movies[lastFilm] = rateOfLastFilm;
-}
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert("You watched a small amaunt of films");
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-        alert("You typical viewer");
-    } else if (personalMovieDB.count > 30) {
-        alert("Woww you such a big lover of cinema");
-    } else {
-        alert("Error");
-    }
-}
+personalMovieDB.writeYourGenres();
+personalMovieDB.showYourGenres();
+console.log(personalMovieDB);
